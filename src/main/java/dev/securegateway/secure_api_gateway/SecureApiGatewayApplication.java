9ef8ce41bc6ea2220c1b8928eeefbd61225942ec
Sanmitha Laxmikanth <sanmithalaxmikanth@gmail.com>
2026-09-17 00:48:11 +0530

    Initial Spring Boot 4.1 skeleton, JDK 25, health check working

---
+++ src/main/java/dev/securegateway/secure_api_gateway/SecureApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.securegateway.secure_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApiGatewayApplication.class, args);
+	}
+
+}
