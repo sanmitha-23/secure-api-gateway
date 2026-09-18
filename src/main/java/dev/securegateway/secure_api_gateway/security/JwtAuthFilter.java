61d44d31293b7ab48bd542d3d9a280f419f77808
Sanmitha Laxmikanth <sanmithalaxmikanth@gmail.com>
2026-09-19 01:50:30 +0530

    Add JwtAuthFilter to establish identity from Bearer tokens

--- src/main/java/dev/securegateway/secure_api_gateway/security/JwtAuthFilter.java
+++ src/main/java/dev/securegateway/secure_api_gateway/security/JwtAuthFilter.java
@@ -1,4 +1,45 @@
 package dev.securegateway.secure_api_gateway.security;
 
-public class JwtAuthFilter {
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Objects;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (Objects.isNull(authHeader) || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+        } else {
+            String token = authHeader.substring(7);
+
+            try {
+                String username = jwtUtil.extractUsername(token);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
+                        Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JwtException e) {
+                logger.debug(e.getMessage());
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
 }
